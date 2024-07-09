266a6563b22dc4b052942fa9b0314c7da4677b1f
Emma <emma.dirscherl@gmail.com>
2024-07-09 12:39:30 +0200

    add feature 2 - spotlight piece

---
+++ components/Spotlight/index.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import styled from "styled-components";
+
+const RandomArt = styled.div`
+  border: 0.2rem solid orange;
+  border-radius: 1rem;
+  margin-bottom: 1rem;
+  padding-top: 1rem;
+  padding-left: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 500px;
+  margin-left: 2.45rem;
+`;
+
+export default function Spotlight({ image, artist, height, width }) {
+  return (
+    <RandomArt>
+      <Image
+        src={image}
+        alt="Image of spotlight art piece"
+        height={height / 10}
+        width={width / 10}
+      />
+      <h3>{artist}</h3>
+    </RandomArt>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import ArtPieces from "@/components/ArtPieces";
+import Spotlight from "@/components/Spotlight";
 import useSWR from "swr";
+import { uid } from "uid";
 
 const fetcher = (...args) => fetch(...args).then((response) => response.json());
 
@@ -14,8 +16,26 @@ export default function HomePage() {
   if (error) {
     return <h1>Something went wrong!</h1>;
   }
+
+  if (!pieces) {
+    return <p>Keine Daten vorhanden</p>;
+  }
+
+  function randomIndex() {
+    return Math.floor(Math.random() * pieces.length);
+  }
+
+  const randomPiece = pieces[randomIndex()];
+
   return (
     <>
+      <Spotlight
+        key={uid()}
+        image={randomPiece.imageSource}
+        artist={randomPiece.artist}
+        height={randomPiece.dimensions.height}
+        width={randomPiece.dimensions.width}
+      />
       <ArtPieces pieces={pieces}></ArtPieces>
     </>
   );
