0cecbe0345af5d8dd158614ba6df16546972cf19
Emma <emma.dirscherl@gmail.com>
2024-07-09 11:40:56 +0200

    add feature 1 - list of all art pieces

---
+++ components/ArtPiecePreview/index.js
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import styled from "styled-components";
+
+const ListItem = styled.li`
+  list-style: none;
+  border: 0.2rem solid black;
+  border-radius: 1rem;
+  margin-bottom: 1rem;
+  padding-top: 1rem;
+  padding-left: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 500px;
+`;
+
+const Artist = styled.h3`
+  margin-top: 0;
+`;
+
+export default function ArtPiecePreview({
+  image,
+  title,
+  artist,
+  height,
+  width,
+}) {
+  return (
+    <ListItem>
+      <Image src={image} alt="hello" height={height / 10} width={width / 10} />
+      <h2>{title}</h2>
+      <Artist>{artist}</Artist>
+    </ListItem>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import ArtPieces from "@/components/ArtPieces";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((response) => response.json());
+
 export default function HomePage() {
+  const url = "https://example-apis.vercel.app/api/art";
+  const { data: pieces, error, isLoading } = useSWR(url, fetcher);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>Something went wrong!</h1>;
+  }
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <>
+      <ArtPieces pieces={pieces}></ArtPieces>
+    </>
   );
 }
